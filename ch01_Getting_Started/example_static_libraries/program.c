8fd56161da2b1ed80a3a695cf07e3effdb7451e9
demon90s <demon90s@163.com>
2017-10-15 22:51:51 -0400

    commit

---
+++ ch01_Getting_Started/example_static_libraries/program.c
@@ -0,0 +1,13 @@
+/*
+ * It includes the library header file and calls one of the functions from the library
+ */
+
+#include <stdlib.h>
+#include "lib.h"
+
+int main(int argc, char* argv[])
+{
+	bill("Hello World");
+
+	exit(0);
+}
