ff16db4e8aa4cd698b9a154fa048e76b0736a77d
demon90s <demon90s@163.com>
2018-12-06 14:05:12 +0800

    commit

---
+++ ch04_Linux环境/showenv.c
@@ -0,0 +1,24 @@
+/*
+    实验 environ 变量
+
+    #include <stdlib.h>
+    extern char **environ;
+
+    这是一个字符串数组( NULL 结尾)，存放了的是环境变量的设定： "var=vale"
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+extern char **environ;
+
+int main()
+{
+    char **env = environ;
+
+    while (*env) {
+        printf("%s\n", *env);
+        env++;
+    }
+    return 0;
+}
---
+++ ch04_Linux环境/user.c
@@ -0,0 +1,15 @@
+/*
+    实验 用户信息
+*/
+
+#include <sys/types.h>
+#include <pwd.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+    
+    return 0;
+}
