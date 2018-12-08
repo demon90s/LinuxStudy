4436470b8756ac58011966991b7cef78ebbd993a
demon90s <demon90s@163.com>
2018-12-08 18:03:55 +0800

    commit

--- ch02_shell程序设计/example_array.sh
+++ ch02_shell程序设计/example_array.sh
@@ -8,17 +8,44 @@
 # 访问元素：${array[i]}
 # 数组长度：${#array[@]}
 
-main_func()
-{
-	array=(Hello World Hi Wow)
-	array[1]=Hey
+array=(Hello World Hi Wow)
 
+# 数组遍历
+traverse_func()
+{
 	local i=0
 	while [ "$i" != ${#array[@]} ]
 	do
 		echo ${array[$i]}
 		i=$((i + 1))
 	done
+}
+
+# 数组查询 参数1是查询内容，返回 index
+find_func()
+{
+	local i=0
+	while [ "$i" != ${#array[@]} ]
+	do
+		if [ "${array[$i]}" = "$1" ]; then
+			return $((i));
+		fi
+		i=$((i + 1))
+	done
+
+	return -1
+}
+
+main_func()
+{
+	array[1]=Hey
+
+	traverse_func
+
+	echo "请输入一个欲查询的内容："
+	read str
+	find_func $str
+	echo "查询到的索引: $?"
 
 	return 0
 }
