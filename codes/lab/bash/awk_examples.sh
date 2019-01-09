42ede88f2259011dd24963a9503ae6828f5a0385
Diwen.Liu <demon90s@163.com>
2019-01-09 20:28:26 +0800

    Deployed fd03bc9 with MkDocs version: 1.0.4

--- codes/lab/bash/awk_examples.sh
+++ codes/lab/bash/awk_examples.sh
@@ -1,6 +1,9 @@
 #! /bin/bash -x
 
+# 
+
 # awk 扫描一个模式，并对符合的模式执行一个动作
+# awk 以行为处理单位，然后根据相应的命令，来处理文本
 
 # 输出匹配的行
 example1()
@@ -14,9 +17,29 @@ tteesstt
 !TEST_FILE!
 }
 
+# 输出分割字符串，默认采取空白分割，使用 -F 'X' 或 FS='X' 表示用 X 分割
+# $0 代表整个行 $1,$2,$3... 分别代表第几个子串
+example2()
+{
+	# 输出每行第二个子串
+	awk '{print $2}' << !TEST_FILE!
+abc world hello
+uuid moc c++
+welldown GOOD bug
+!TEST_FILE!
+
+	# 输出每行第二个子串，使用的是:分割
+	awk -F ':' '{print $2}' << !TEST_FILE!
+abc:world:hello
+uuid:moc:c++
+welldown:GOOD:bug
+!TEST_FILE!
+}
+
 main()
 {
-	example1
+	#example1
+	example2
 }
 
 main
