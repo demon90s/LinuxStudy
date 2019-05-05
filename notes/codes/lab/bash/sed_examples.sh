f7eb1a7e704264402e71f1b624ff5bd43095abec
Diwen.Liu <demon90s@163.com>
2019-05-05 15:29:21 +0800

    commit

--- notes/codes/lab/bash/sed_examples.sh
+++ notes/codes/lab/bash/sed_examples.sh
@@ -20,10 +20,20 @@ example2()
 	cat $this_file | sed '1,12d'
 }
 
+# 显示第几行的内容
+example3()
+{
+	this_file=$0
+
+	# 输出第6到第12行的内容, -n意思是不把原文件内容输出出来, 默认会输出
+	cat $this_file | sed -n '6,12p'
+}
+
 main()
 {
 	#example1
-	example2 $0
+	#example2 $0
+	example3 $0
 }
 
 main $@
