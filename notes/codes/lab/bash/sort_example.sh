b0e4766f85950eeee90d309b31972a7a85a7ff9e
Diwen.Liu <demon90s@163.com>
2019-05-28 20:09:47 +0800

    commit

--- notes/codes/lab/bash/sort_example.sh
+++ notes/codes/lab/bash/sort_example.sh
@@ -12,9 +12,19 @@ a
 !TEST_FILE!
 }
 
+example2()
+{
+	# 输出文件大小的排序
+	# -n 表示用数字排序
+	# -r 表示逆序（从大到小）
+	# -k 5 表示以第5栏排序
+	ls -l | sort -nrk 5
+}
+
 main()
 {
-	example1
+	#example1
+	example2
 }
 
 main
